cec3c3dde116e70ee4d2409d9947d5ad6f0cf7b0
Crysart <idukrystal@gmail.com>
2023-08-04 21:55:33 +0100

    t1 01

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+''' A module to house the concat function
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    ''' concatinates string str1 to
+    string str2 and returns the result
+    '''
+    return a+b
