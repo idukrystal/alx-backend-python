62baf61f47bf94cff39383b765d5ceb0d8d5a881
Crysart <idukrystal@gmail.com>
2023-08-04 22:12:10 +0100

    t1 03

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+''' A module to house the to_str function
+'''
+
+
+def to_str(n: float) -> str:
+    ''' Returns the strinf representstion
+    of float: n
+    '''
+    return str(n)
