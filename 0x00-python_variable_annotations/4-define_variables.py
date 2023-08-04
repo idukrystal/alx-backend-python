069e6956e0af9d183c1695c3af622b27bb04f332
Crysart <idukrystal@gmail.com>
2023-08-04 22:21:28 +0100

    t2 p5

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
+''' An type anotates integer '''
 a: int = 1
+
+''' A type annotated float '''
 pi: float = 3.14
+
+''' A type annotated boolean '''
 i_understand_annotations: bool = True
+
+''' A type anotated string '''
 school: str = "Holberton"
