4a12fa065030ad59f19f0f034138bfaee98c7f73
Crysart <idukrystal@gmail.com>
2023-08-04 22:49:06 +0100

    t1 06

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+''' A module to housecthe sum_mixed_list function
+'''
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    ''' Sums up all the values in
+    list of floats/integers  and return sum
+    '''
+    total: float = 0.0
+    for n in input_list:
+        total += n
+    return total
