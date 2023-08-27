b4796c1812ee7f981587b57654607b433accc4e9
Crysart <idukrystal@gmail.com>
2023-08-27 22:34:24 +0100

    test utils t5

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """ Module to test Utils Module """
 
+import requests
 from parameterized import parameterized
-from unittest import TestCase
-from utils import access_nested_map
+from unittest import TestCase, mock
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(TestCase):
-    """ To test utills.access_nested_map methos """
+    """ To test utills.access_nested_map method """
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -27,3 +28,32 @@ class TestAccessNestedMap(TestCase):
         with self.assertRaises(KeyError) as except_cm:
             access_nested_map(nested_map, path)
         # self.assertEqual(except_cm.exception.args, (msg,))
+
+
+class TestGetJson(TestCase):
+    """ To test utills.get_json method  """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, url, payload):
+        """ function to test get_json results """
+        with mock.patch(
+            "requests.get",
+            return_value=mock_request_get(payload),
+        ) as mock_response:
+            self.assertEquals(get_json(url), payload)
+
+
+def mock_request_get(fake_response):
+    """ Reusable Mock fuction to replace requests.get """
+    class MockResponse:
+        """ Mock Response object """
+
+        def __init__(self, response):
+            self.response = response
+
+        def json(self):
+            return self.response
+    return MockResponse(fake_response)
