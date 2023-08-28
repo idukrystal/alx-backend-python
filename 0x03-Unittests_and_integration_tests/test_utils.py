f96b53cbd45d358ff9c8843c7258413809c495ed
Crysart <idukrystal@gmail.com>
2023-08-28 06:49:53 +0100

    kolgoy

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -60,6 +60,31 @@ class TestGetJson(TestCase):
             self.assertEquals(get_json(url), payload)
 
 
+class TestMemoize(TestCase):
+    """ to test utills.memoize decorator """
+
+    def test_memoize(self) -> None:
+        """ Test the memoise decorators posutively """
+        class TestClass:
+            """ a test class """
+            def a_method(self):
+                """ a method """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """ a property """
+                return self.a_method()
+        with mock.patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+        ) as mock_fn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+        mock_fn.assert_called_once()
+
 def mock_request_get(fake_response) -> "MockResponse":
     """ Reusable Mock fuction to replace requests.get """
     class MockResponse:
